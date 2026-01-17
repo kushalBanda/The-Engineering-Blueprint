94c5e8ee4fb5b43f8d7a072115d2f2b3065e27be
Kushal Banda <kushalbanda265@gmail.com>
2026-01-17 22:50:40 +0530

    Add maxSubArray.java implementation for LeetCode problem 53, providing a solution for maximum subarray sum calculation with a main method for testing.

---
+++ LeetCode/53. Maximum Subarray/maxSubArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = 0;
+
+        for (int num : nums) {
+            currentSum = Math.max(num, currentSum + num);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(solution.maxSubArray(nums));
+    }
+}
