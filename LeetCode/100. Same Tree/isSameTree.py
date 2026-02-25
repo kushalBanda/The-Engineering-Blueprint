115fa272f046c417293e2d285440ecdc1727493d
Kushal Banda <kushalbanda265@gmail.com>
2026-02-25 22:40:06 +0530

    Add Python implementation for Same Tree problem, including logic to compare two binary trees for equality, and a corresponding PNG asset for visual documentation.

---
+++ LeetCode/100. Same Tree/isSameTree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+solution = Solution()
+print(solution.isSameTree(TreeNode(1, TreeNode(2), TreeNode(3)), TreeNode(1, TreeNode(2), TreeNode(3))))
